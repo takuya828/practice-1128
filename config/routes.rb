44dc3ab6d1ceefc9659233559e3194c77b955a0b
takuya828 <takuya11070221@gmail.com>
2022-02-12 03:16:16 +0000

    [Add]Javaプログラムコード集作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get '/java' => 'homes#java', as: 'java'
   get '/java2' => 'homes#java2', as: 'java2'
+  get '/java3' => 'homes#java3', as: 'java3'
 
 end
