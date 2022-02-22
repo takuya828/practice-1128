e2f0d1938c776de7012dfd9c44e8ebe9afa76309
takuya828 <takuya11070221@gmail.com>
2022-02-22 13:32:24 +0000

    [Add]Javaの学習記録③ページを作成、内部クラスについて記述

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   get '/java2' => 'homes#java2', as: 'java2'
   get '/java3' => 'homes#java3', as: 'java3'
   get '/php' => 'homes#php', as: 'php'
+  get '/java4' => 'homes#java4', as: 'java4'
+  
 
 end
